444a5debe940a1f724db70f7f5caaf1dc69ff4d3
Fernando Martinez <fernando817mm@gmail.com>
2021-06-17 14:02:35 -0500

    initial commit

--- index.js
+++ index.js
@@ -40,7 +40,9 @@ class Airplane {
       - Give instances of Person a method `.toString()`:
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
-  
+
+//redundant comment
+
  class Person {
     
   }
